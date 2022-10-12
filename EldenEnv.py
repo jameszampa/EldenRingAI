189af025cab1f15ecb54543d5a52836d453da4d3
James Zampa <james.zampa@gmail.com>
2022-10-12 12:29:29 -0500

    dont return to grace if fighting boss

--- EldenEnv.py
+++ EldenEnv.py
@@ -111,7 +111,7 @@ class EldenEnv(gym.Env):
 
         if not self.death:
             # Time limit for fighting Tree sentienel (600 seconds or 10 minutes)
-            if (time.time() - self.t_start) > TOTAL_ACTIONABLE_TIME:
+            if (time.time() - self.t_start) > TOTAL_ACTIONABLE_TIME and self.rewardGen.time_since_seen_boss > 2.5:
                 headers = {"Content-Type": "application/json"}
                 requests.post(f"http://{self.agent_ip}:6000/action/return_to_grace", headers=headers)
                 self.done = True
