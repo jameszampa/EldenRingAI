e9b4fca1601ee693f0851165e9c21acb2a90f37e
James Zampa <james.zampa@gmail.com>
2022-10-15 15:17:02 -0500

    looked at boss hp

--- show_rewards.py
+++ show_rewards.py
@@ -1,20 +1,21 @@
 import cv2
 from EldenReward import EldenReward
 import time
+import numpy as np
 
 cap = cv2.VideoCapture('/dev/video0')
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 
-rewardGen = EldenReward(1)
+rewardGen = EldenReward(1, "templogs")
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    runes, stat, hp, death, dmg_reward, find_reward = rewardGen.update(frame)
+    runes, stat, hp, death, dmg_reward, find_reward, time_since_seen_boss = rewardGen.update(frame)
     print("Runes:", runes)
     print("Stat:", stat)
     print("HP:", hp)
@@ -23,5 +24,24 @@ while True:
     print('find_reward', find_reward)
     print(rewardGen.current_stats)
     print(rewardGen.seen_boss)
-    cv2.imshow('er', frame[800:850, 800:1100])
-    cv2.waitKey(1)
+    # cv2.imshow('er', )
+    if rewardGen.seen_boss:
+        hp_image = frame[865:875, 465:1460]
+        p_count = 0
+        for i in range(int(1460 - 465)):
+            avg = 0
+            for j in range(10):
+                r = np.float64(hp_image[j, (1460 - 465 - 1) - i][0])
+                g = np.float64(hp_image[j, (1460 - 465 - 1) - i][1])
+                b = np.float64(hp_image[j, (1460 - 465 - 1) - i][2])
+                avg = np.float64((r + g + b)) / 3
+            if (avg / 10) > 4:
+                p_count += 1
+            if p_count > 15:
+                print(f'boss_hp: {(((1460 - 465 - 1) - i) / (1460 - 465))}')
+                break
+        if p_count < 15:
+            print('boss_hp:1')
+    else:
+        print('boss_hp : 1')
+    #cv2.waitKey(1)
