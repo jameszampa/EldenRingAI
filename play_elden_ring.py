79c35eb9a88b08a08a89c8a41aea71b4e00c28ff
James Zampa <james.zampa@gmail.com>
2022-10-20 13:49:56 -0500

    faster steps

--- play_elden_ring.py
+++ play_elden_ring.py
@@ -301,7 +301,7 @@ def init_fight():
             elden_agent.keyboard.release('a')
 
             elden_agent.keyboard.press('w')
-            time.sleep(2)
+            time.sleep(0.5)
             elden_agent.keyboard.release('w')
 
             
