fd8394f3d1e8a6afdb76394972d1a1023c6f5567
James Zampa <james.zampa@gmail.com>
2022-10-27 10:42:39 -0500

    better init

--- play_elden_ring.py
+++ play_elden_ring.py
@@ -271,6 +271,7 @@ def init_fight():
             elden_agent.keyboard.press('f')
             time.sleep(0.05)
             elden_agent.keyboard.release('f')
+            time.sleep(2)
             elden_agent.keyboard.release(kb.Key.space)
             elden_agent.keyboard.release('w')
 
