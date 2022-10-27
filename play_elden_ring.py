b5ce1ce9f00acde5256bb7b0c7c57de5de728393
James Zampa <james.zampa@gmail.com>
2022-10-27 13:23:39 -0500

    added jumpingh

--- play_elden_ring.py
+++ play_elden_ring.py
@@ -271,7 +271,7 @@ def init_fight():
             update_status(f'Initializing fight')
             elden_agent.keyboard.press('w')
             elden_agent.keyboard.press(kb.Key.space)
-            time.sleep(2.25)
+            time.sleep(2.30)
             elden_agent.keyboard.press('f')
             time.sleep(0.05)
             elden_agent.keyboard.release('f')
